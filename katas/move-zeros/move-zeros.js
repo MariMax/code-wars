b81335d6e9ce82a0f4b14e596be5fe4d84a6a334
Maxim <mma29121983@gmail.com>
2015-10-12 15:21:44 +0300

    Update move-zeros.js

--- katas/move-zeros/move-zeros.js
+++ katas/move-zeros/move-zeros.js
@@ -1,5 +1,16 @@
-function moveZeros() {
-
+function move_zeros(arrNum, isRight = true){
+  const zeroArray = [];
+  let i = 0;
+  while (i<arrNum.length){
+    if (!arrNum[i]){
+      zeroArray.push(arrNum[i]);
+      arrNum.splice(i,1);
+      continue;
+    }
+    i+=1;
+  }
+  
+  return isRight?arrNum.concat(zeroArray):zeroArray.concat(arrNum);
 }
 
 //module.exports = moveZeros;
